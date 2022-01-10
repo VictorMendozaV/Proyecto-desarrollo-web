5006bc789924edfea7c85463f89c5ceedf092eb8
Victor Mendoza <90165909+VictorMendozaV@users.noreply.github.com>
2022-01-10 17:14:15 -0500

    Las clases ya funcionan
    guarda los datos del jugador en una clase llamada player

--- actualizar.js
+++ actualizar.js
@@ -1,30 +1,28 @@
 
 
 
-
+window.onload = function(){  
 
 class players{
 
-    players(name, score, time){
+    constructor(name , score , time) {
         this._name = name;
         this._score = score;
-        this._time  = time; 
+        this._time = time;
+ 
     }
 
 
     get Name(){
-         console.log("entra name: "+this._name);
-        return this._name;; 
+        return this._name;
     }
 
     get  Score(){
-        console.log("entra score: "+this._score);
-        return  this._score;;
+        return  this._score;
     }
 
     get Time(){
-        console.log("entra time: "+this._time);
-        return this._time;;
+        return this._time;
     }
 
     set Name(name){
@@ -44,19 +42,18 @@ class players{
 
 
 
-
-let n = localStorage.getItem("name");
-let sc = localStorage.getItem("score");
-let  t = localStorage.getItem("time");
+let n = localStorage.getItem('name');
+let sc = localStorage.getItem('score');
+let  t = localStorage.getItem('time');
 
  
- 
+let p  = new players( n, sc, t);
 
-window.onload = function(){  
 
-    let p  = new players( n, sc, t);
-    document.getElementById("printScore").innerHTML = p.Name + " tu puntuacion es: ";
-    document.getElementById("printName").innerHTML = p.Score;
-    document.getElementById("printTime").innerHTML =" tiempo: "+p.Time;
+
+   
+    document.getElementById("printName").innerHTML = p.Name + " tu puntuacion es: ";
+    document.getElementById("printScore").innerHTML = p.Score;
+    document.getElementById("printTime").innerHTML =" tiempo: "+ p.Time;
 
 }
